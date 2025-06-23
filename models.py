6ef159a2bca307095d01370159e63a9ce459e146
Tamil-god <m.naruto2009@gmail.com>
2025-06-23 15:13:17 +0530

    Delete models.py

--- models.py
+++
@@ -1,9 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-db = SQLAlchemy()
-
-class Message(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), nullable=False)
-    text = db.Column(db.Text, nullable=False)
-    timestamp = db.Column(db.String(50), nullable=False)
